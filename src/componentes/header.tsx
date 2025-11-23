11dafefea844489184317eb15edf35e9c38c443e
Amanda Trinity <amandatrinity16@gmail.com>
2025-11-23 20:08:48 -0300

    feature: cria a estrutura do header

---
+++ src/componentes/header.tsx
@@ -0,0 +1,27 @@
+import PhotoMy from "../assets/my.jpeg"
+import "../styles/header.css"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="imagem">
+                <img src={PhotoMy} alt="Foto pessoal" />
+            </div>
+            <div className="texto">
+                <div className="frase">
+                    <h2>"O SUCESSO É A SOMA DE PEQUENOS ESFORÇOS REPETIDOS"</h2>
+                </div>
+                <div className="nome">
+                    <h1>Amanda Trinity</h1>
+                </div>
+                <div className="botoes">
+                    <button className="botao">Sobre Mim</button>
+                    <button className="botao">Projetos</button>
+                    <button className="botao">Contato</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
