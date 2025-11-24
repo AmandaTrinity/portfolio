f01b4e4e701e9a833a4e1bae4d149bb1834f78ed
Amanda Trinity <amandatrinity16@gmail.com>
2025-11-23 23:20:45 -0300

    feature(App): renderiza os componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import Header from "./componentes/header"
+import Sobre from "./componentes/sobre"
+import Formacao from "./componentes/formacao"
+import { Projects  } from "./componentes/projetos"
+import Contato from "./componentes/contato"
+
 const App = () => {
   return (
     <>
-      App
+      <Header />
+      <Sobre />
+      <Formacao />
+      <Projects />
+      <Contato />
     </>
+
   )
 }
 
